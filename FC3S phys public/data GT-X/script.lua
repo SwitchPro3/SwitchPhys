ab609b56056180d11fa4366e585fa97a5dfa1858
SwitchPro <trashemail123412341234@gmail.com>
2025-04-02 12:32:24 -0400

    Add files via upload

--- FC3S phys public/data GT-X/script.lua
+++ FC3S phys public/data GT-X/script.lua
@@ -7,12 +7,12 @@ local function safe_require(module_name)
     return value
   end
 
-local switch_throttle_model = safe_require("script_switch_throttle_model")
+local jpg_throttle_model = safe_require("script_jpg_throttle_model")
 local ustahl_turbo_ref_model = safe_require("script_ustahl_turbo_ref_model")
 
 function script.update(dt)
-    if switch_throttle_model then
-        switch_throttle_model.runThrottleModel()
+    if jpg_throttle_model then
+        jpg_throttle_model.runThrottleModel()
     end
     if ustahl_turbo_ref_model then
         ustahl_turbo_ref_model.runTurboRefModel()
